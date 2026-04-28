723eb09b4e013a6998b9b9a51441561594217362
Jonel <jonelpronton06@gmail.com>
2026-04-28 16:29:32 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,37 +4,7 @@ import SearchBar from "./components/searchBar";
 export default function App() {
   return (
     <div
-      style={{
-        width: "100%",
-        minHeight: "100vh",
-        background: "#ffffff",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "20px",
-        gap: "10px",
-      }}
-    >
-      <SearchBar />
-
-      <BrowseCard
-        title="Ilanora"
-        description="Explore thousands of high-quality card list ui images on Dribbble. Your resource to get inspired, discover and connect with designers worldwide."
-        onClick={() => alert("Test")}
-      />   
-      
-
-        <BrowseCard
-          title="Ilanora"
-          description="Explore thousands of high-quality card list ui images on Dribbble. Your resource to get inspired, discover and connect with designers worldwide."
-          onClick={() => alert("Test")}
-        />      
-   
-
-
-
-
+    
     </div>
 
   );
