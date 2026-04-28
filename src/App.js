ddac8324f494a6635444461393e4ed3633c2929b
Jonel <jonelpronton06@gmail.com>
2026-04-28 16:27:38 +0800

    browse page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import BrowseCard from "./components/BrowseCard";
+import SearchBar from "./components/searchBar";
+
 export default function App() {
   return (
-    <>
-      <h1 className="text-green-500">PELEC202 + ITPS205 Finals Project</h1>
-      <p>Group 3</p>
-    </>
+    <div
+      style={{
+        width: "100%",
+        minHeight: "100vh",
+        background: "#ffffff",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "20px",
+        gap: "10px",
+      }}
+    >
+      <SearchBar />
+
+      <BrowseCard
+        title="Ilanora"
+        description="Explore thousands of high-quality card list ui images on Dribbble. Your resource to get inspired, discover and connect with designers worldwide."
+        onClick={() => alert("Test")}
+      />   
+      
+
+        <BrowseCard
+          title="Ilanora"
+          description="Explore thousands of high-quality card list ui images on Dribbble. Your resource to get inspired, discover and connect with designers worldwide."
+          onClick={() => alert("Test")}
+        />      
+   
+
+
+
+
+    </div>
+
   );
 }
