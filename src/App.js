61a6c5ec112ce8d138015ace58833d79b392625b
khianvictorycalderon <ckhianvictory@gmail.com>
2026-04-29 09:36:54 +0800

    Added Routers + Pages components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import BrowsePage from "./pages/BrowsePage";
+import RecordPage from "./pages/RecordPage";
+import ErrorPage from "./pages/ErrorPage";
+import Layout from "./pages/Layout";
+
 function App() {
   return (
-    <div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<LandingPage />} />
+        <Route path="/browse" element={<BrowsePage />} />
+        <Route path="/record/:id" element={<RecordPage />} />
+        <Route path="*" element={<ErrorPage/>} />
+      </Route>
+    </Routes>
   );
 }
 
